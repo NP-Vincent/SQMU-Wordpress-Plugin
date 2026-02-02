46d8894160bfe60bdc88b630a53a92b4d8780e67
NP-Vincent <n.p.vincent@icloud.com>
2026-02-02 21:55:35 +0400

    Delete src directory

--- src/lib/ethers.js
+++
@@ -1,8 +0,0 @@
-import { ethers as importedEthers } from 'ethers';
-
-export const getEthers = () => {
-  if (typeof window !== 'undefined' && window.ethers) {
-    return window.ethers;
-  }
-  return importedEthers;
-};
